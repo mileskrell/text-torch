a8d877503bca030234c3b91665af1ea14a1613d0
Miles Krell <noreply@mileskrell.com>
2018-11-24 01:44:10 -0500

    Implement Up navigation
    The only user-facing change is that the "about" fragment now provides Up
    navigation.

--- app/src/main/java/com/example/miles/whotextsfirst/MainActivity.kt
+++ app/src/main/java/com/example/miles/whotextsfirst/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.miles.whotextsfirst
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController, AppBarConfiguration(setOf(R.id.intro_dest, R.id.main_dest)))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
