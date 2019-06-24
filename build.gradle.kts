5038c7e22047e961f28f02e216d5029fe3eceae9
Miles Krell <noreply@mileskrell.com>
2019-06-24 02:02:53 -0400

    Update to Kotlin 1.3.40

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.1")
-        classpath(kotlin("gradle-plugin", "1.3.30"))
+        classpath(kotlin("gradle-plugin", "1.3.40"))
 
         val navigationVersion by extra("2.0.0")
         // NOTE: Do not place your application dependencies here; they belong
