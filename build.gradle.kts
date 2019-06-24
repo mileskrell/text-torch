a9eb325a3f04ef4cb7e6f7c552e12588bcd716fb
Miles Krell <noreply@mileskrell.com>
2019-06-24 01:58:48 -0400

    Update Gradle and Android Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.2")
+        classpath("com.android.tools.build:gradle:3.4.1")
         classpath(kotlin("gradle-plugin", "1.3.30"))
 
         val navigationVersion by extra("2.0.0")
