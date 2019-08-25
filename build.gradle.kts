d63e0241f1110e177b7ed1383b5339acb6398692
Miles Krell <noreply@mileskrell.com>
2019-08-25 18:06:14 -0400

    Update: Gradle 5.4.1, Android Gradle plugin 3.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.2")
+        classpath("com.android.tools.build:gradle:3.5.0")
         classpath(kotlin("gradle-plugin", "1.3.41"))
 
         val navigationVersion by extra("2.0.0")
