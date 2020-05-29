55a013a1fffbabb87359bac0b80a914be95f3e7d
Miles Krell <noreply@mileskrell.com>
2020-05-29 00:07:10 -0400

    Add copyright and license notices to all Kotlin source files

--- app/src/main/java/com/mileskrell/texttorch/about/LicensesFragment.kt
+++ app/src/main/java/com/mileskrell/texttorch/about/LicensesFragment.kt
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020 Miles Krell and the Text Torch contributors
+ *
+ * This file is part of Text Torch.
+ *
+ * Text Torch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Text Torch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Text Torch.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.mileskrell.texttorch.about
 
 import android.os.Bundle
