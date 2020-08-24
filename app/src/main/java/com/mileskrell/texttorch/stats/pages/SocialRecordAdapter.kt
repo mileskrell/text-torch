37fd273bcd6a42055cfef2d814f7f146238db56f
Miles Krell <noreply@mileskrell.com>
2020-08-24 12:42:46 -0400

    Remove SocialRecord card animation
    It was cool, but I discovered that it was sometimes buggy. I'm planning
    to replace it with a simpler animation in the next commit.

--- app/src/main/java/com/mileskrell/texttorch/stats/pages/SocialRecordAdapter.kt
+++ app/src/main/java/com/mileskrell/texttorch/stats/pages/SocialRecordAdapter.kt
@@ -19,7 +19,6 @@
 
 package com.mileskrell.texttorch.stats.pages
 
-import android.animation.ValueAnimator
 import android.graphics.Typeface
 import android.text.Spannable
 import android.text.SpannableStringBuilder
@@ -28,16 +27,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
 import com.mileskrell.texttorch.R
 import com.mileskrell.texttorch.stats.model.SocialRecord
 import kotlinx.android.synthetic.main.social_record_view_holder.view.*
 
 class SocialRecordAdapter(val type: SocialRecordAdapterType) : RecyclerView.Adapter<SocialRecordAdapter.SocialRecordViewHolder>() {
-
-    private var lastLoadTime = System.nanoTime()
-    private val timeElapsed
-        get() = (System.nanoTime() - lastLoadTime) / 1_000_000
 
     private var socialRecords = listOf<SocialRecord>()
 
@@ -56,7 +52,6 @@ class SocialRecordAdapter(val type: SocialRecordAdapterType) : RecyclerView.Adap
 
     fun loadSocialRecords(socialRecords: List<SocialRecord>) {
         this.socialRecords = socialRecords
-        this.lastLoadTime = System.nanoTime()
         notifyDataSetChanged()
     }
 
@@ -86,7 +81,7 @@ class SocialRecordAdapter(val type: SocialRecordAdapterType) : RecyclerView.Adap
                 itemView.correspondent_name_address_text_view.text = record.correspondentName ?: record.correspondentAddress
             }
 
-            val endPosition = when (type) {
+            when (type) {
                 SocialRecordAdapterType.WHO_TEXTS_FIRST -> {
                     itemView.center_text_view.text =
                         itemView.resources.getQuantityString(
@@ -99,7 +94,9 @@ class SocialRecordAdapter(val type: SocialRecordAdapterType) : RecyclerView.Adap
                     itemView.you_data_text_view.text =
                         itemView.context.getString(R.string.texted_first_x_percent_of_the_time, 100 - record.correspondentInitPercent)
 
-                    record.correspondentInitPercent / 100f
+                    itemView.divider.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                        horizontalBias = record.correspondentInitPercent / 100f
+                    }
                 }
                 SocialRecordAdapterType.TOTAL_TEXTS -> {
                     itemView.center_text_view.text =
@@ -113,7 +110,9 @@ class SocialRecordAdapter(val type: SocialRecordAdapterType) : RecyclerView.Adap
                     itemView.you_data_text_view.text =
                         itemView.context.getString(R.string.sent_x_percent_of_texts, 100 - record.correspondentTextPercent)
 
-                    record.correspondentTextPercent / 100f
+                    itemView.divider.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                        horizontalBias = record.correspondentTextPercent / 100f
+                    }
                 }
                 SocialRecordAdapterType.AVERAGE_LENGTH -> {
                     itemView.center_text_view.text =
@@ -127,35 +126,10 @@ class SocialRecordAdapter(val type: SocialRecordAdapterType) : RecyclerView.Adap
                     itemView.you_data_text_view.text =
                         itemView.context.getString(R.string.x_characters_on_average, record.ownAvgChars)
 
-                    record.correspondentAvgCharsPercent / 100f
-                }
-            }
-
-            val dividerLayoutParams = itemView.divider.layoutParams as ConstraintLayout.LayoutParams
-
-            val animationLength = itemView.resources.getInteger(R.integer.divider_animation_length_ms)
-
-            if (timeElapsed < animationLength) {
-                // Determine proper start position (in between 0.5 and endPosition).
-                // If all items started at 0.5, any items that weren't initially visible would animate too fast.
-                val startPosition = if (endPosition >= 0.5) {
-                    0.5f + (timeElapsed.toFloat() / animationLength * (endPosition - 0.5f))
-                } else {
-                    0.5f - (timeElapsed.toFloat() / animationLength * (0.5f - endPosition))
-                }
-
-                ValueAnimator.ofFloat(startPosition, endPosition).apply {
-                    // Only animate until lastLoadTime + animationLength
-                    duration = animationLength - timeElapsed
-                    addUpdateListener {
-                        dividerLayoutParams.horizontalBias = animatedValue as Float
-                        itemView.divider.layoutParams = dividerLayoutParams
+                    itemView.divider.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                        horizontalBias = record.correspondentAvgCharsPercent / 100f
                     }
-                    start()
                 }
-            } else {
-                dividerLayoutParams.horizontalBias = endPosition
-                itemView.divider.layoutParams = dividerLayoutParams
             }
         }
     }
