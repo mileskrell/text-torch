47447f24346d7f442f9e06a4dc7111a21ce053bb
Miles Krell <noreply@mileskrell.com>
2020-08-25 21:38:40 -0400

    Bump: ConstraintLayout 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     val navigationVersion = "2.3.0"
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0")
     implementation("androidx.core:core-ktx:1.3.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
     implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
