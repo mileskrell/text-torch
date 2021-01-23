2c475120b34533d87a986f49d8e79dd58fa37b11
Miles Krell <repos@mileskrell.com>
2021-01-22 21:14:49 -0500

    Version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.mileskrell.texttorch"
         minSdkVersion(21)
         targetSdkVersion(28)
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
