55334c7cabf419b4b223d8e3282dfd1e1833faf7
Miles Krell <noreply@mileskrell.com>
2021-05-12 17:13:14 -0400

    Add explicit dependency on androidx.recyclerview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
     implementation("androidx.preference:preference-ktx:1.1.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.0")
 
     val aboutLibrariesVersion: String by rootProject.extra
     implementation("com.google.android.material:material:1.2.1")
