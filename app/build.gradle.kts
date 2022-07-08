d9efd86c87b67a36e82ad20477c6b42348a05b31
Miles Krell <noreply@mileskrell.com>
2022-07-08 15:30:24 -0400

    Update to core-ktx 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
