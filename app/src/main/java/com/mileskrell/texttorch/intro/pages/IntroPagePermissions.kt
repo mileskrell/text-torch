mileskrell/text-torch

commit fe23040d5f5d994f3f5b6c7062f0c56acd48ba5b

Switch to ViewPager2 in IntroFragment

app/src/main/java/com/mileskrell/texttorch/intro/pages/IntroPagePermissions.kt
@@ -87,17 +87,6 @@ class IntroPagePermissions : Fragment(R.layout.fragment_intro_page_permissions) 
                 )
             )
         }
-
-        // I think storing permission statuses in a ViewModel would just introduce more complexity,
-        // so after configuration changes, we need to check them again.
-        if (readSmsGranted() && readContactsGranted()) {
-            // TODO: Move this check to onStart() or onResume(). Then we can detect if
-            //  the user is returning after granting the permissions in the app's settings.
-            // I haven't done this yet because it results in a "FragmentManager is already
-            // executing transactions" error. Related: see BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
-            // at the moment, fragments in the ViewPager are capped at STARTED.
-            onPermissionsGranted()
-        }
     }
 
     private fun onPermissionsGranted() {

app/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
-    implementation("androidx.viewpager:viewpager:1.0.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     implementation("com.google.android.material:material:1.6.1")

app/src/main/java/com/mileskrell/texttorch/intro/IntroFragment.kt
@@ -34,7 +34,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import androidx.preference.PreferenceManager
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.mileskrell.texttorch.MainActivity
 import com.mileskrell.texttorch.R
 import com.mileskrell.texttorch.analyze.AnalyzeFragment
@@ -50,7 +50,8 @@ import io.sentry.Sentry
 
 class IntroFragment : Fragment(R.layout.fragment_intro) {
 
-    private lateinit var introPagerAdapter: IntroPagerAdapter
+    private var _binding: FragmentIntroBinding? = null
+    private val b get() = _binding!!
 
     private val introViewModel: IntroViewModel by activityViewModels()
     private var hasSeenTutorial: Boolean? = null // Because primitives can't be lateinit
@@ -123,7 +124,7 @@ class IntroFragment : Fragment(R.layout.fragment_intro) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val b = FragmentIntroBinding.bind(view)
+        _binding = FragmentIntroBinding.bind(view)
         // This check is needed because onViewCreated() is called even if we've already seen the
         // tutorial (since navigating to the next fragment takes a moment)
         if (hasSeenTutorial == false) {
@@ -131,16 +132,13 @@ class IntroFragment : Fragment(R.layout.fragment_intro) {
         }
 
         b.introViewPager.offscreenPageLimit = IntroViewModel.PAGE.values().size
-        b.introViewPager.adapter = IntroPagerAdapter(introViewModel, childFragmentManager).also {
-            introPagerAdapter = it
-        }
+        b.introViewPager.adapter = IntroPagerAdapter(introViewModel, this)
         val colorBackground = (b.introViewPager.background as LayerDrawable)
             .getDrawable(0).mutate() as ColorDrawable
         val logoBackground = (b.introViewPager.background as LayerDrawable)
             .getDrawable(1).mutate() as BitmapDrawable
 
-        b.introViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrollStateChanged(state: Int) {}
+        b.introViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageScrolled(
                 position: Int,
                 positionOffset: Float,
@@ -163,33 +161,37 @@ class IntroFragment : Fragment(R.layout.fragment_intro) {
 
                 // Since we don't add pages until the user is allowed to go to them, there's no
                 // danger in showing this button while the last page is partly visible.
-                if (position == (b.introViewPager.adapter as IntroPagerAdapter).count - 1) {
-                    b.introArrowNext.hide()
-                } else {
-                    b.introArrowNext.show()
-                }
+                refreshFabVisibility()
             }
 
             override fun onPageSelected(position: Int) {
-                val adapter = b.introViewPager.adapter as IntroPagerAdapter
                 Sentry.addBreadcrumb(
-                    "[$TAG] Switched intro page to " + adapter.getItem(position)::class.simpleName
+                    "[$TAG] Switched to intro page at index ${b.introViewPager.currentItem}"
                 )
             }
         })
 
-        // TODO: Can we make this button scroll more slowly?
         b.introArrowNext.setOnClickListener {
-            b.introViewPager.arrowScroll(View.FOCUS_RIGHT)
+            b.introViewPager.currentItem = b.introViewPager.currentItem + 1
+        }
+    }
+
+    fun refreshFabVisibility() {
+        if (b.introViewPager.currentItem == b.introViewPager.adapter!!.itemCount - 1) {
+            b.introArrowNext.hide()
+        } else {
+            b.introArrowNext.show()
         }
     }
 
     fun ensureAnalyticsPageAdded() {
-        introPagerAdapter.ensureAnalyticsPageAdded()
+        (b.introViewPager.adapter as IntroPagerAdapter).ensureAnalyticsPageAdded()
+        refreshFabVisibility()
     }
 
     fun ensureEnterAppPageAdded() {
-        introPagerAdapter.ensureEnterAppPageAdded()
+        (b.introViewPager.adapter as IntroPagerAdapter).ensureEnterAppPageAdded()
+        refreshFabVisibility()
     }
 
     /**
@@ -218,6 +220,12 @@ class IntroFragment : Fragment(R.layout.fragment_intro) {
                 inclusive = true
             }
         })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        b.introViewPager.adapter = null
+        _binding = null
     }
 
     companion object {

app/src/main/java/com/mileskrell/texttorch/intro/IntroPagerAdapter.kt
@@ -19,59 +19,49 @@
 
 package com.mileskrell.texttorch.intro
 
-import android.view.ViewGroup
+import android.os.Build
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.mileskrell.texttorch.intro.pages.IntroPageAnalytics
 import com.mileskrell.texttorch.intro.pages.IntroPageEnterApp
 import com.mileskrell.texttorch.intro.pages.IntroPagePermissions
 import com.mileskrell.texttorch.intro.pages.IntroPageWelcome
 
-class IntroPagerAdapter(private val introViewModel: IntroViewModel, fm: FragmentManager) :
-    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-
-    private val pages = mutableListOf<Fragment>(IntroPageWelcome()).apply {
-        if (introViewModel.permissionsPageAdded) {
-            add(IntroPagePermissions())
-        }
-        if (introViewModel.analyticsPageAdded) {
-            add(IntroPageAnalytics())
-        }
-        if (introViewModel.enterAppPageAdded) {
-            add(IntroPageEnterApp())
-        }
-    }
+class IntroPagerAdapter(private val introViewModel: IntroViewModel, fragment: Fragment) :
+    FragmentStateAdapter(fragment) {
 
     fun ensureAnalyticsPageAdded() {
-        if (pages.none { it::class == IntroPageAnalytics::class }) {
+        if (!introViewModel.analyticsPageAdded) {
             introViewModel.analyticsPageAdded = true
-            pages.add(IntroPageAnalytics())
-            notifyDataSetChanged()
+            notifyItemInserted(itemCount)
         }
     }
 
     fun ensureEnterAppPageAdded() {
-        if (pages.none { it::class == IntroPageEnterApp::class }) {
+        if (!introViewModel.enterAppPageAdded) {
             introViewModel.enterAppPageAdded = true
-            pages.add(IntroPageEnterApp())
-            notifyDataSetChanged()
+            notifyItemInserted(itemCount)
         }
     }
 
-    override fun getItem(position: Int) = pages[position]
-
-    override fun getCount() = pages.size
-
-    /**
-     * Fix intro fragment references after configuration changes.
-     *
-     * See https://stackoverflow.com/a/17629575
-     */
-    // TODO: Hold on, I'm actually not sure if I need this. Figure out what's going on here.
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        return super.instantiateItem(container, position).also {
-            pages[position] = it as Fragment
-        }
+    override fun getItemCount(): Int {
+        return 1 +
+                (if (introViewModel.permissionsPageAdded) 1 else 0) +
+                (if (introViewModel.analyticsPageAdded) 1 else 0) +
+                (if (introViewModel.enterAppPageAdded) 1 else 0)
     }
+
+    override fun createFragment(position: Int) =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) when (position) {
+            0 -> IntroPageWelcome()
+            1 -> IntroPageAnalytics()
+            2 -> IntroPageEnterApp()
+            else -> throw RuntimeException()
+        } else when (position) {
+            0 -> IntroPageWelcome()
+            1 -> IntroPagePermissions()
+            2 -> IntroPageAnalytics()
+            3 -> IntroPageEnterApp()
+            else -> throw RuntimeException()
+        }
 }

app/src/main/res/layout/fragment_intro.xml
@@ -8,15 +8,11 @@
     android:fitsSystemWindows="true"
     tools:context=".intro.IntroFragment">
 
-    <androidx.viewpager.widget.ViewPager
+    <androidx.viewpager2.widget.ViewPager2
         android:id="@+id/intro_view_pager"
         android:layout_width="match_parent"
         android:layout_height="match_parent"
-        android:background="@drawable/intro_view_pager_background"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toTopOf="parent" />
+        android:background="@drawable/intro_view_pager_background" />
 
     <com.google.android.material.floatingactionbutton.FloatingActionButton
         android:id="@+id/intro_arrow_next"

commit c5eafe90a8e0bc31a8955f0791d8e59624aa7c78

Don't show "grant permissions" button on config changes
Fixes a bug introduced in fe23040d

app/src/main/java/com/mileskrell/texttorch/intro/pages/IntroPagePermissions.kt
@@ -26,9 +26,11 @@ import android.text.method.LinkMovementMethod
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.mileskrell.texttorch.R
 import com.mileskrell.texttorch.databinding.FragmentIntroPagePermissionsBinding
 import com.mileskrell.texttorch.intro.IntroFragment
+import com.mileskrell.texttorch.intro.IntroViewModel
 import com.mileskrell.texttorch.util.*
 import io.sentry.SentryLevel
 
@@ -36,6 +38,8 @@ class IntroPagePermissions : Fragment(R.layout.fragment_intro_page_permissions) 
 
     private var _binding: FragmentIntroPagePermissionsBinding? = null
     private val b get() = _binding!!
+
+    private val introViewModel: IntroViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -78,6 +82,12 @@ class IntroPagePermissions : Fragment(R.layout.fragment_intro_page_permissions) 
             }
         }
 
+        // TODO: Also adding the analytics page here on app startup would be nice, but
+        //  it results in a "FragmentManager is already executing transactions" error.
+        if (readSmsGranted() && readContactsGranted() && introViewModel.analyticsPageAdded) {
+            indicatePermissionsGranted()
+        }
+
         b.introPermissionsButton.setOnClickListener {
             logToBoth(TAG, "Clicked \"grant needed permissions\" button")
             requestPermissionsLaucher.launch(
@@ -90,9 +100,13 @@ class IntroPagePermissions : Fragment(R.layout.fragment_intro_page_permissions) 
     }
 
     private fun onPermissionsGranted() {
+        indicatePermissionsGranted()
+        (parentFragment as IntroFragment).ensureAnalyticsPageAdded()
+    }
+
+    private fun indicatePermissionsGranted() {
         b.introPermissionsButton.visibility = View.INVISIBLE
         b.introTextViewPermissionsGranted.visibility = View.VISIBLE
-        (parentFragment as IntroFragment).ensureAnalyticsPageAdded()
     }
 
     override fun onDestroyView() {